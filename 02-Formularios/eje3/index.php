79a02d3394263c0f6e31120d9209f426d08c6fca
DariaKuznetsovaS <daria.kuznetsova.50758@ikasle.egibide.org>
2026-09-18 14:06:19 +0200

    rellelno de datos

--- 02-Formularios/eje3/index.php
+++ 02-Formularios/eje3/index.php
@@ -1,5 +1,10 @@
 <?php
 if(isset($_POST["asunto"])&&isset($_POST["email"])&&isset($_POST["motivo"])&&isset($_POST["mensaje"])){
+$asunto=$_POST["asunto"];
+$email=$_POST["email"];
+$motivo=$_POST["motivo"];
+$mensaje=$_POST["mensaje"];
+
 }
 
 require "index.view.php";
